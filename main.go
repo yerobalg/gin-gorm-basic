d7ce4fe9762e13468bdb86bb240f6ca2932734e7
dnabil <daffanabilhakim@gmail.com>
2023-02-26 16:33:32 +0700

    add seed for category

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"gin-gorm-basic/sdk/sql"
 	"gin-gorm-basic/src/business/entity"
 	"gin-gorm-basic/src/handler"
+
+	"gorm.io/gorm"
 )
 
 func main() {
@@ -32,9 +34,38 @@ func main() {
 	db := sql.GetInstance()
 	db.AutoMigrate(entity.Post{}, &entity.Comment{}, &entity.Category{})
 
+	if err := seedCategory(db); err != nil {
+		panic("GAGAL SEED BROOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO")
+	}
+
 	handler := handler.Init(conf, db)
 	handler.Run()
+}
 
 
+func seedCategory(sql *gorm.DB) error{
+	var categories []entity.Category
 
+	if err := sql.First(&categories).Error; err != gorm.ErrRecordNotFound {
+		return err
+	}
+	categories = []entity.Category{
+		{
+			Name: entity.BusinessCategory,
+		},
+		{
+			Name: entity.TravelCategory,
+		},
+		{
+			Name: entity.HealthCategory,
+		},
+		{
+			Name: entity.TechnologyCategory,
+		},
+	}
+
+	if err := sql.Create(&categories).Error; err != nil {
+		return err
+	}
+	return nil
 }
--- src/business/entity/category.go
+++ src/business/entity/category.go
@@ -1,5 +1,12 @@
 package entity
 
+var (
+	HealthCategory     = "Health"
+	TechnologyCategory = "Technology"
+	BusinessCategory   = "Business"
+	TravelCategory     = "Travel"
+)
+
 type Category struct {
 	ID   uint
 	Name string `gorm:"type:VARCHAR(30)"`
